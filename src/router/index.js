3c05b4e55ce393febab0d545b79a19f23595a276
Marija <pesicmaria94@gmail.com>
2020-05-09 16:23:02 +0200

    CreateReservationView added to the router

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import LandPageView from "../views/LandPageView.vue";
 import RegisterView from "../views/RegisterView.vue";
 import ResidencesView from "../views/ResidencesView.vue";
 import ProfileView from "../views/ProfileView.vue";
+import CreateReservationView from "../views/CreateReservationView.vue";
 
 Vue.use(VueRouter);
 
@@ -45,6 +46,14 @@ const routes = [
     path: "/my_profile",
     name: "profile",
     component: ProfileView,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: "/view_residence",
+    name: "view_residence",
+    component: CreateReservationView,
     meta: {
       requiresAuth: true
     }
