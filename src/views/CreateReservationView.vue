2b7bca995322b6d03cf270fa3a7b15c16213624d
Marija <pesicmaria94@gmail.com>
2020-05-11 21:24:13 +0200

    Create reservation page created

--- src/views/CreateReservationView.vue
+++ src/views/CreateReservationView.vue
@@ -2,22 +2,101 @@
     <div fluid>
         <div class="bg" overlay="black-strong">
             <div class="overlay"></div>
-            <Navbar />
-            Create Reservation
-            {{ residence.type }}
         </div>
+            <Navbar />
+            <mdb-container style="margin-top: 10px">
+                <mdb-row>
+                    <mdb-col col="6" id="r_view" class="animated slideInLeft"> 
+                        <mdb-input label="Type" icon="home" type="text" name="type" v-model="residence.type" disabled/>
+                        <mdb-input label="Address" icon="map-marker-alt" type="text" name="address" v-model="residence.address" disabled/>
+                        <mdb-input label="Town" icon="city" type="text" name="town" v-model="residence.town" disabled/>
+                        <mdb-input label="Country" icon="globe-europe" type="text" name="country" v-model="residence.country" disabled/>
+                        <mdb-input label="Price" icon="dollar-sign" type="text" name="price" v-model="residence.price" disabled/>
+                        <mdb-input label="Bed number" icon="bed" type="number" name="bed_number"  v-model="residence.bed_number" disabled/>
+                        <mdb-input icon="pencil-alt" wrapperClass="active-pink-textarea" type="textarea" label="Description" name="description" v-model="residence.description" disabled/>
+                    </mdb-col>
+                    <mdb-col col="6" id="create" class="animated slideInRight">
+                        <mdb-container id="info">
+                            <h5>{{ residence.type }} owner info:</h5>
+                            <hr>
+                            <mdb-row>
+                                <mdb-col col="2">
+                                    <mdb-icon icon="user" size="lg" style="margin-top:25px" />
+                                </mdb-col>
+                                <mdb-col col="10">
+                                    <mdb-input label="First name:" type="text" name="first_name" v-model="residence.owner.first_name" size="sm" disabled style="width:50%"/>
+                                </mdb-col>
+                            </mdb-row>
+                            <mdb-row>
+                                <mdb-col col="2">
+                                    <mdb-icon icon="user" size="lg" style="margin-top:25px" />
+                                </mdb-col>
+                                <mdb-col col="10">
+                                    <mdb-input label="Last name:" type="text" name="last_name" v-model="residence.owner.last_name" size="sm" disabled style="width:50%" />
+                                </mdb-col>
+                            </mdb-row>
+                            <mdb-row>
+                                <mdb-col col="2">
+                                    <mdb-icon icon="at" size="lg" style="margin-top:25px" />
+                                </mdb-col>
+                                <mdb-col col="10">
+                                    <mdb-input label="E-mail:" type="text" name="conatct" v-model="residence.owner.email" size="sm" disabled style="width:50%"/>
+                                </mdb-col>
+                            </mdb-row>    
+                        </mdb-container> 
+                        <mdb-container id="date_picker" style="margin-top: 2px; margin-bottom: 2px;">
+                            <mdb-row>
+                                <mdb-col>
+                                    <h5>Choose date:</h5>
+                                    <vc-date-picker v-model="range" mode="range" is-inline/>
+                                </mdb-col>
+                                <mdb-col class="btn-center">
+                                    <mdb-btn gradient="morpheus-den" type="button" v-on:click="reserve" class="rounded">Create reservation</mdb-btn>
+                                </mdb-col>
+                            </mdb-row>
+                        </mdb-container>
+                    </mdb-col>
+                </mdb-row>
+            </mdb-container>
     </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue"
+import { mdbContainer, mdbRow, mdbCol, mdbInput, mdbIcon, mdbBtn } from 'mdbvue';
 export default {
     components: {
         Navbar,
+        mdbContainer,
+        mdbRow,
+        mdbCol,
+        mdbInput,
+        mdbIcon,
+        mdbBtn
     },
     computed: {
         residence() {
             return this.$store.getters.getResidence
+        }
+    }, 
+    data() {
+        return {
+            range: {
+                start: new Date(),
+                end: new Date()
+            },
+            //id: residence.id
+        }
+    },
+    methods: {
+        reserve: function() {
+            console.log(this.range.start.getFullYear()+"-"+(this.range.start.getMonth()+1)+"-"+this.range.start.getDate())
+            console.log(this.residence.id)
+            this.$store.dispatch("create_reservation", {
+                id: this.residence.id,
+                date_from: this.range.start.getFullYear()+"-"+(this.range.start.getMonth()+1)+"-"+this.range.start.getDate(),
+                date_to: this.range.end.getFullYear()+"-"+(this.range.end.getMonth()+1)+"-"+this.range.end.getDate()
+            })
         }
     }
 }
@@ -49,4 +128,37 @@ export default {
     opacity: .2;
     background: #8585ad;
 }
+
+    #info {
+        width: 100%;
+        height: 51%;
+        margin-top: 5px;
+        background-color: rgba(255, 255, 255, 0.4);
+        border-radius: 15px;
+        text-align: center;
+        padding: 5px;
+    }
+
+    hr {
+        width: 50%;
+        color: rgba(255, 255, 255, 0.9);
+    }
+
+    #r_view {
+        border-radius: 10px;
+        background-color: rgba(255, 255, 255, 0.4);
+    }
+
+    #create {
+        border-radius: 10px;
+        background-color: rgba(244, 67, 54, 0.3);
+        text-align: center;
+        
+    }
+    .btn-center {
+        position:absolute;                  
+        bottom:0;                          
+        left: 25%;
+    }
+   
 </style>
