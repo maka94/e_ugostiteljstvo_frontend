9e491e735b2d4fa3accf6428289bed46aa7ce681
Marija <pesicmaria94@gmail.com>
2020-05-11 21:25:59 +0200

    DatePicker

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <vc-date-picker v-model="range" mode="range" is-inline/>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "datepicker",
+    data() {
+        return {
+            range: {
+                start: new Date(),
+                end: new Date()
+            }
+        };
+    },
+    
+}
+</script>
+
+<style scoped>
+  </style>
