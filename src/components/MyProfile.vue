ac82b8053410b353077dd583d63bf8a0bd57614e
Marija <pesicmaria94@gmail.com>
2020-05-16 13:58:00 +0200

    edit profile and change password

--- src/components/MyProfile.vue
+++ src/components/MyProfile.vue
@@ -1,23 +1,37 @@
 <template>
     <div>
-        <mdb-container fluid>
+        <mdb-container fluid style="padding: 10px;">
+            <div id="change_pass" v-if="change_password" class="animated slideInUp">
+                <mdb-input label="Enter your old password" v-model="password.old_password" type="password"></mdb-input>
+                <mdb-input label="Enter new password" v-model="password.new_password" type="password"> </mdb-input>
+                <mdb-input label="Confirm new password" v-model="password.confirm" type="password"> </mdb-input>
+                    
+                <mdb-btn gradient="young-passion" v-on:click="change_password=false" style="width: 30%">Close</mdb-btn>
+                <mdb-btn v-on:click="modal=true, change_password=false" style="left: 30%;">Save changes</mdb-btn>  
+            </div>
             <mdb-row>
-                <mdb-col xs="12" sm="6" md="4">
+               
+                <mdb-col>
+                    slika
                 </mdb-col>
-                <mdb-col xs="12" sm="6" md="8">
-                    <mdb-card>
-                        <mdb-card-title> Profile  </mdb-card-title>
-                        <mdb-card-body>
-                            <form action="#">
-                                <div class="grey-text">
-                                    <mdb-input label="First name" v-model="profile.first_name"></mdb-input>
-                                    <mdb-input label="Last name" v-model="profile.last_name"> </mdb-input>
-                                    <mdb-input label="Username" v-model="profile.username"> </mdb-input>
-                                    <mdb-input label="E-mail" v-model="profile.email"> </mdb-input>
-                                </div>
-                            </form>
-                        </mdb-card-body>
-                    </mdb-card>
+                <mdb-col>
+                    <div class="grey-text">
+                        <mdb-input label="First name" v-model="profile.first_name"></mdb-input>
+                        <mdb-input label="Last name" v-model="profile.last_name"> </mdb-input>
+                        <mdb-input label="Username" v-model="profile.username"> </mdb-input>
+                        <mdb-input label="E-mail" v-model="profile.email"> </mdb-input>
+                    </div> 
+                    <mdb-btn type="button" gradient="aqua" v-on:click="modal=true, update=true" id="edit_btn">Edit profile</mdb-btn> 
+                    <mdb-btn type="button" gradient="amy-crisp" v-on:click="change_password=true" id="edit_btn">Change password</mdb-btn>
+                    <mdb-modal :show="modal" @close="modal = false">
+                        <mdb-modal-header>
+                            <mdb-modal-title>Are you sure you want to save changes?</mdb-modal-title>
+                        </mdb-modal-header>
+                        <mdb-modal-footer>
+                            <mdb-btn color="secondary" @click.native="modal = false, update=false">Close</mdb-btn>
+                            <mdb-btn color="primary" v-on:click="updateProfile">Save changes</mdb-btn>
+                        </mdb-modal-footer>
+                    </mdb-modal>   
                 </mdb-col>
             </mdb-row>
         </mdb-container>
@@ -25,17 +39,20 @@
 </template>
 
 <script>
-import { mdbContainer, mdbRow, mdbCol, mdbCard, mdbInput, mdbCardTitle, mdbCardBody } from 'mdbvue';
+import { mdbContainer, mdbRow, mdbCol,  mdbInput, mdbBtn, mdbModal, mdbModalHeader, mdbModalTitle, mdbModalFooter } from 'mdbvue';
+import toast from "@/assets/js/services/toast";
 export default {
     name: "profile",
     components: {
         mdbContainer,
         mdbRow,
         mdbCol,
-        mdbCard,
         mdbInput,
-        mdbCardTitle,
-        mdbCardBody
+        mdbBtn, 
+        mdbModal, 
+        mdbModalHeader, 
+        mdbModalTitle,
+        mdbModalFooter
     },
     data() {
         return {
@@ -44,17 +61,44 @@ export default {
                 last_name: "",
                 username: "",
                 email: ""
-            }
+            },
+            password: {
+                old_password: "",
+                new_password: "",
+                confirm: ""
+            },
+            modal: false,
+            update: false,
+            change_password: false
         }
+    },
+    created() {
+        this.$store.dispatch("getProfile")
+        .then(response => (this.profile = response))
     },
     methods: {
         updateProfile(){
-            this.$store.dispatch("getProfile")
-            .then(response => (this.profile = response));
+            if(this.update){
+
+                if(this.profile.first_name && this.profile.last_name && this.profile.username && this.profile.email){
+                    this.$store.dispatch("updateProfile", this.profile)
+                    .then(response => (this.profile = response))
+                    this.modal = false;
+                    this.update = false;
+                } else {
+                    if(!this.profile.first_name) toast.error("First name cannot be empty!")
+                    if(!this.profile.last_name) toast.error("Last name cannot be empty!")
+                    if(!this.profile.username) toast.error("Username cannot be empty!")
+                    if(!this.profile.email) toast.error("Email cannot be empty!")
+                } 
+            } else {
+                if(this.password.old_password && this.password.new_password && this.password.confirm) {
+                    this.$store.dispatch("changePassword", this.password)
+                    .then(this.change_password = false)
+                    this.modal = false
+                }
+            }
         }
-    },
-    async mounted(){
-        this.updateProfile()
     }
 }
 </script>
@@ -65,9 +109,27 @@ export default {
         text-align: center;
     }
 
-    .row #card {
-        border-width: 5%;
-        border-color: slategrey;
+    .grey-text {
+        background-color: rgba(255, 235, 59, 0.3);
+        border-radius: 25px;
+        padding: 10px;
+        width: 70%;
     }
+
+    #edit_btn {
+        right: 14%;
+        width: 30%;
+    }
+
+    #change_pass {
+        background-color: rgba(255, 255, 255, 0.4);
+        border-radius: 20px;
+        width: 35%;
+        height: 20%;
+        padding: 10px;
+        margin-top: 15px;
+        margin: auto;
+    }
+   
 
 </style>
