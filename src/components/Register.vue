6e257b49522c23d80c1be622efb047375e7bce81
Marija <pesicmaria94@gmail.com>
2020-05-16 13:59:41 +0200

    style fixed

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,7 +6,7 @@
         <mdb-col col="4">
           <mdb-card id="register">
             <mdb-card-body>
-                <form action="#" @submit="validate">
+                <form action="#" @submit="register">
                   <p class="h4 text-center mb-4">Register</p>
                   <div class="grey-text">
                       <mdb-input label="Enter first name" icon="user" type="text" name="firstname" id="firstname" v-model="firstname"/>
@@ -25,48 +25,14 @@
         <mdb-col col="4"></mdb-col>
       </mdb-row>
     </mdb-container>
-       <!-- <form action="#" @submit="validate">
-            <h2 class="login-heading">Register</h2>
-
-              <p v-if="errors.length">
-                <b>Please correct the following error(s):</b>
-                <ul>
-                    <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-                </ul>
-            </p>
-
-            
-            <p>
-                <label for="firstname">First name</label>
-                <input id="firstname" v-model="firstname" type="text" name="firstname">
-            </p>
-            <p>
-                <label for="lastname">Last name</label>
-                <input id="lastname" v-model="lastname" type="text" name="lastname">
-            </p>
-            <p>
-                <label for="username">Username</label>
-                <input id="username" v-model="username" type="text" name="username">
-            </p>
-            <p>
-                <label for="email">Email</label>
-                <input id="email" v-model="email" type="email" name="email">
-            </p>
-            <p>
-                <label for="password">Password</label>
-                <input id="password" v-model="password" type="password" name="password">
-            </p>
-            <p>
-                <input type="submit" value="Submit">
-            </p>
-        </form>-->
     </div>
 </template>
 
 <script>
 import { mdbContainer, mdbRow, mdbCol, mdbCard, mdbCardBody, mdbInput, mdbBtn } from 'mdbvue';
+import toast from "@/assets/js/services/toast";
 export default {
-    components: {
+     components: {
         mdbContainer,
         mdbRow, 
         mdbCol,
@@ -86,30 +52,8 @@ export default {
         }
     },
     methods:{
-        validate: function(e){
-            if(this.firstname && this.lastname && this.username && this.email && this.password){
-                this.register();
-            }
-            this.errors = [];
-
-            if(!this.firstname){
-                this.errors.push('First name is required!');
-            }
-            if(!this.lastname){
-                this.errors.push('Last name is required!');
-            }
-            if(!this.username){
-                this.errors.push('Usrename is required!');
-            }
-            if(!this.email){
-                this.errors.push('Email is required!');
-            }
-            if(!this.password){
-                this.errors.push('Password is required!')
-            }
-            e.preventDefault();
-        },
         register(){
+            if(this.firstname && this.lastname && this.username && this.email && this.password){
             this.$store.dispatch('register', {
                 firstname: this.firstname,
                 lastname: this.lastname,
@@ -117,14 +61,19 @@ export default {
                 email: this.email,
                 password: this.password,
             })
+
             .then(
                this.$router.push({name: 'login'})
                 //this.successMessage = "Registered successfully!"
             )
-            .catch(error => {
-                this.$router.push({ name: 'register' })
-                this.errors = Object.values(error.response.data.username)
-            })
+            } else {
+                if(!this.firstname) toast.error("First name is required")
+                if(!this.lastname) toast.error("Last name is required")
+                if(!this.username) toast.error("Username is required")
+                if(!this.email) toast.error("Email is required")
+                if(!this.password) toast.error("Password is required")
+            }
+            
         }
     }
 }
