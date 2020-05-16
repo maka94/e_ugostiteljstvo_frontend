11393703512d31864f0e6e43225e64e98ac7a025
Marija <pesicmaria94@gmail.com>
2020-05-16 14:15:06 +0200

    components imported, style defined

---
+++ src/views/ReservationsView.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div class="bg" overlay="black-strong">
+            <div class="overlay"></div>
+            <Navbar />
+            <Reservations style="margin-top: 15px" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue"
+import Reservations from "@/components/Reservations.vue"
+export default {
+    components: {
+        Navbar,
+        Reservations
+    }
+}
+</script>
+
+<style scoped>
+    .bg {
+    background-image: url("../assets/land_page_background.jpg");
+    height: 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+    background-attachment: fixed;
+  }
+
+  .bg .overlay {
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    opacity: .2;
+    background: #8585ad;
+}
+</style>
