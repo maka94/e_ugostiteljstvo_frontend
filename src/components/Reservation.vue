94ecfacbc23ce3db1582d545a294da5b29f612f0
Marija <pesicmaria94@gmail.com>
2020-05-18 22:41:52 +0200

    Residence pictures added

--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -17,7 +17,17 @@
                     </div>
                 </mdb-col>
                 <mdb-col>
-                    Slike
+                    <mdb-container id="info">
+                        <h5>Owner info:</h5>
+                        <mdb-row class="info">
+                        <h6> Name: {{ reservation.residence.owner.first_name }} </h6>
+                        <h6> {{ reservation.residence.owner.last_name }} </h6> 
+                        </mdb-row>
+                        <mdb-row class="info">
+                            <h6> E-mail: {{ reservation.residence.owner.email }} </h6>
+                        </mdb-row>
+                    </mdb-container>
+                    <mdb-carousel id="images" :interval="8000" slide :items="items" controlls indicators></mdb-carousel>
                 </mdb-col>
             </mdb-row>
         </mdb-container>
@@ -25,18 +35,29 @@
 </template>
 
 <script>
-import { mdbContainer, mdbRow, mdbCol, mdbBtn, mdbInput } from 'mdbvue';
+import { mdbContainer, mdbRow, mdbCol, mdbBtn, mdbInput, mdbCarousel } from 'mdbvue';
 export default {
     components: {
         mdbContainer,
         mdbRow, 
         mdbCol,
         mdbBtn,
-        mdbInput
+        mdbInput,
+        mdbCarousel
     },
     computed: {
         reservation() {
             return this.$store.getters.getReservation
+        },
+        items() {
+            var images =  []
+            this.reservation.residence.images.forEach(getImage)
+            function getImage(value) {
+                console.log(value.image)
+                var image  = {img:true, src: "http://localhost:8000/residences/download/"+value.image, mask: "rgba-purple-slight"}
+                images.push(image)
+            }
+            return images
         }
     },
     methods: {
@@ -53,4 +74,21 @@ export default {
         background-color: rgba(255, 255, 255, 0.4);
         border-radius: 25px;
     }
+
+    h5 {
+        text-align: center;
+    }
+
+    .info {
+        padding: 10px;
+    }
+
+    #info {
+        background-color: rgba(255, 255, 255, 0.4);
+        margin-bottom: 20px;
+        border-radius: 25px;
+        padding: 10px;
+    }
+
+
 </style>
