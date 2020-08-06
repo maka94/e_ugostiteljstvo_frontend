013ac35712527891b640410fde2fee0d01703333
Marija <pesicmaria94@gmail.com>
2020-08-06 18:11:15 +0200

    Background color for dropdown menu changed

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,7 +1,7 @@
 <template>
 <mdb-dropdown btn-group dropleft style="margin-left: 200px; z-index: 999;">
-    <mdb-dropdown-toggle id="dropdown" slot="toggle" color="secondary"><mdb-icon icon="user"/></mdb-dropdown-toggle>
-    <mdb-dropdown-menu>
+    <mdb-dropdown-toggle id="dropdown" color="default" slot="toggle"><mdb-icon icon="user"/></mdb-dropdown-toggle>
+    <mdb-dropdown-menu style="z-index: 999; background-color: #e0f2f1 ;">
       <mdb-dropdown-item name="residences" id="residences" v-on:click="showResidences">My Residences</mdb-dropdown-item>
       <mdb-dropdown-item name="reservations" id="reservations" v-on:click="showReservations">My Reservations</mdb-dropdown-item>
       <mdb-dropdown-item name="myprofile" id="myprofile" v-on:click="showProfile">My Profile</mdb-dropdown-item>
@@ -44,5 +44,9 @@ export default {
 <style scoped>
 #dropdown:hover{
   opacity: 1.0;
+}
+
+#dropdown {
+  
 }
 </style>
