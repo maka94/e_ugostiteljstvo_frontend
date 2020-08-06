03b01bd6357ae770527d24600434546f592fef9d
Marija <pesicmaria94@gmail.com>
2020-08-06 18:13:40 +0200

    Reordering of page elements and background image removed

--- src/views/CreateReservationView.vue
+++ src/views/CreateReservationView.vue
@@ -1,77 +1,103 @@
 <template>
-    <div fluid>
-        <div class="bg" overlay="black-strong">
-            <div class="overlay"></div>
-        </div>
+    <mdb-container>
             <Navbar />
-            <mdb-container style="margin-top: 10px">
-                <mdb-row>
-                    <mdb-col col="6" id="r_view" class="animated slideInLeft"> 
-                        <mdb-input label="Type" icon="home" type="text" name="type" v-model="residence.type" disabled/>
-                        <mdb-input label="Address" icon="map-marker-alt" type="text" name="address" v-model="residence.address" disabled/>
-                        <mdb-input label="Town" icon="city" type="text" name="town" v-model="residence.town" disabled/>
-                        <mdb-input label="Country" icon="globe-europe" type="text" name="country" v-model="residence.country" disabled/>
-                        <mdb-input label="Price per day" icon="dollar-sign" type="text" name="price" v-model="residence.price" disabled/>
-                        <mdb-input label="Bed number" icon="bed" type="number" name="bed_number"  v-model="residence.bed_number" disabled/>
-                        <mdb-input icon="pencil-alt" wrapperClass="active-pink-textarea" type="textarea" label="Description" name="description" v-model="residence.description" disabled/>
-                    </mdb-col>
-                    <mdb-col col="6" id="create" class="animated slideInRight">
-                        <mdb-container id="info">
-                            <h5>{{ residence.type }} owner info:</h5>
-                            <hr>
-                            <mdb-row>
-                                <mdb-col col="2">
-                                    <mdb-icon icon="user" size="lg" style="margin-top:25px" />
-                                </mdb-col>
-                                <mdb-col col="10">
-                                    <mdb-input label="First name:" type="text" name="first_name" v-model="residence.owner.first_name" size="sm" disabled style="width:50%"/>
-                                </mdb-col>
-                            </mdb-row>
-                            <mdb-row>
-                                <mdb-col col="2">
-                                    <mdb-icon icon="user" size="lg" style="margin-top:25px" />
-                                </mdb-col>
-                                <mdb-col col="10">
-                                    <mdb-input label="Last name:" type="text" name="last_name" v-model="residence.owner.last_name" size="sm" disabled style="width:50%" />
-                                </mdb-col>
-                            </mdb-row>
-                            <mdb-row>
-                                <mdb-col col="2">
-                                    <mdb-icon icon="at" size="lg" style="margin-top:25px" />
-                                </mdb-col>
-                                <mdb-col col="10">
-                                    <mdb-input label="E-mail:" type="text" name="conatct" v-model="residence.owner.email" size="sm" disabled style="width:50%"/>
-                                </mdb-col>
-                            </mdb-row>    
-                        </mdb-container> 
-                        <mdb-container id="date_picker" style="margin-top: 2px; margin-bottom: 2px;">
+            <br/>
+            <mdb-row>
+                <mdb-col lg="5" md="5" s="5" xs="12" >
+                    <mdb-carousel id="images" :interval="8000" slide :items="items" controlls indicators></mdb-carousel>
+                </mdb-col>
+                <mdb-col lg="2"></mdb-col>
+                <mdb-col lg="5" md="5" s="5" xs="12">
+                    <div>
+                        <mdb-container id="date_picker">
                             <mdb-row>
                                 <mdb-col>
                                     <h5>Choose date:</h5>
                                     <vc-date-picker v-model="range" mode="range" is-inline/>
                                 </mdb-col>
-                                <mdb-col class="btn-center">
-                                    <mdb-input label="Number of days:" type="number" size="sm" v-model="days" style="width:30%; left: 30%;" />
-                                    <mdb-input label="Total:" type="number" size="sm" v-model="total_price" style="width:30%; left: 30%" />
-                                    <mdb-btn gradient="morpheus-den" type="button" v-on:click="reserve" class="rounded">Create reservation</mdb-btn>
+                                <mdb-col>
+                                    <br>
+                                    <mdb-input label="Nights:" type="number" size="sm" v-model="days" />
+                                    <mdb-input label="Total:" type="number" size="sm" v-model="total_price"  />
+                                </mdb-col>
+                            </mdb-row>
+                            <mdb-row>
+                                <mdb-col>
+                                    <br>
+                                    <div class="d-flex justify-content-center">
+                                        <mdb-btn size="lg" 
+                                        gradient="morpheus-den" 
+                                        type="button" 
+                                        class="btn float-left" 
+                                        style="border-radius: 5px;"
+                                        v-on:click="reserve">
+                                        Create reservation
+                                        </mdb-btn>
+                                    </div>
                                 </mdb-col>
                             </mdb-row>
                         </mdb-container>
-                    </mdb-col>
-                </mdb-row>
+                    </div>
+                </mdb-col>
+            </mdb-row>
+            <br><hr style="width:100%;">
+            <mdb-row>
+                <mdb-col col id="r_view" class="animated slideInLeft"> 
+                    <mdb-input label="Type" icon="home" type="text" name="type" v-model="residence.type" disabled/>
+                    <mdb-input label="Address" icon="map-marker-alt" type="text" name="address" v-model="residence.address" disabled/>
+                    <mdb-input label="Town" icon="city" type="text" name="town" v-model="residence.town" disabled/>
+                    <mdb-input label="Country" icon="globe-europe" type="text" name="country" v-model="residence.country" disabled/>
+                    <mdb-input label="Price per night" icon="dollar-sign" type="text" name="price" v-model="residence.price" disabled/>
+                    <mdb-input label="Bed number" icon="bed" type="number" name="bed_number"  v-model="residence.bed_number" disabled/>
+                    <mdb-input icon="pencil-alt" wrapperClass="active-pink-textarea" type="textarea" label="Description" name="description" v-model="residence.description" disabled/>
+                </mdb-col>
+            </mdb-row>
+            
+            <mdb-row>
+                <mdb-col col id="create" class="animated slideInRight">
+                    <mdb-container id="info">
+                        <h5>{{ residence.type }} owner info:</h5>
+                        <hr>
+                        <mdb-row>
+                            <mdb-col col="2">
+                                <mdb-icon icon="user" size="lg" style="margin-top:25px" />
+                            </mdb-col>
+                            <mdb-col col="10">
+                                <mdb-input label="First name:" type="text" name="first_name" v-model="residence.owner.first_name" size="sm" disabled style="width:50%"/>
+                            </mdb-col>
+                        </mdb-row>
+                        <mdb-row>
+                            <mdb-col col="2">
+                                <mdb-icon icon="user" size="lg" style="margin-top:25px" />
+                            </mdb-col>
+                            <mdb-col col="10">
+                                <mdb-input label="Last name:" type="text" name="last_name" v-model="residence.owner.last_name" size="sm" disabled style="width:50%" />
+                            </mdb-col>
+                        </mdb-row>
+                        <mdb-row>
+                            <mdb-col col="2">
+                                <mdb-icon icon="at" size="lg" style="margin-top:25px" />
+                            </mdb-col>
+                            <mdb-col col="10">
+                                <mdb-input label="E-mail:" type="text" name="conatct" v-model="residence.owner.email" size="sm" disabled style="width:50%"/>
+                            </mdb-col>
+                        </mdb-row>    
+                    </mdb-container> 
+                    
+                </mdb-col>
+            </mdb-row>
                 <mdb-row>
                     <mdb-col col="1"></mdb-col>
                     <mdb-col col="10">
-                        <h4>Gallery</h4>
-                        <mdb-carousel id="images" :interval="8000" slide :items="items" controlls indicators></mdb-carousel>
+                        
                     </mdb-col>
                     <mdb-col col="1"></mdb-col>
                 </mdb-row>
                 <mdb-row>
                     <google-map v-bind:lat="Number(this.residence.lat)" v-bind:lon="Number(this.residence.lon)" v-bind:reservation="true"></google-map>
                 </mdb-row>
-            </mdb-container>
-    </div>
+            
+    </mdb-container>
 </template>
 
 <script>
@@ -136,31 +162,6 @@ export default {
 </script>
 
 <style scoped>
-.bg {
-    background-image: url("../assets/land_page_background.jpg");
-    height: 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-
-    position: fixed;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    z-index: -1;
-    background-attachment: fixed;
-  }
-
-  .bg .overlay {
-    position: fixed;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    opacity: .2;
-    background: #8585ad;
-}
 
     #info {
         width: 100%;
@@ -196,6 +197,8 @@ export default {
     
     #images {
         margin: auto;
+        height: 200px;
+        width: 600px;
     }
 
     h4 {
@@ -204,4 +207,14 @@ export default {
         border-radius: 15px;
         margin-top: 10px;
     }
+
+    #date_picker {
+        border: 1px solid rgb(221, 221, 221);
+        border-radius: 12px;
+        padding: 20px;
+        box-shadow: rgba(0, 0, 0, 0.12) 0px 6px 16px;
+    }
+
+    
+
 </style>
