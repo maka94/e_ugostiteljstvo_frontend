5e835642cf5297e20768e68d99dcef0d9b902859
Marija <pesicmaria94@gmail.com>
2020-08-10 21:59:01 +0200

    css removed

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -18,30 +18,3 @@ export default {
 }
 </script>
 
-<style scoped>
-    .bg {
-    background-image: url("../assets/land_page_background.jpg");
-    height: 100%;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-
-    position: fixed;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    z-index: -1;
-    background-attachment: fixed;
-  }
-
-  .bg .overlay {
-    position: fixed;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    opacity: .2;
-    background: #8585ad;
-}
-</style>
